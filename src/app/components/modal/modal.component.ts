62a64d4903ade05ab01ec84cf88f8f8ea650439f
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2022-07-02 23:55:06 +0300

    create registration number select

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ICarInfo } from '../../interfaces/car-info.interface';
 
 @Component({
   selector: 'app-modal',
@@ -13,6 +14,20 @@ export class ModalComponent {
     shDriverName: false
   }
   showDriverNameEl = false;
+  showRegistrationOptions = false;
+  options: ICarInfo[] = [
+    { id: 1, imageUrl: '../../../assets/image/BMW-X3.png', registration: 'AT 1505 BM', make: 'BMW', model: 'X3' },
+    { id: 2, imageUrl: '', registration: 'KA 1234 KA', make: 'Toyota', model: 'Prius' }
+  ]
+
+  @ViewChild('optionsDiv') optionsDiv!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  clickOut(event: MouseEvent) {
+    if (!this.optionsDiv.nativeElement.contains(event.target)) {
+      this.showRegistrationOptions = false
+    }
+  }
 
   form: FormGroup = new FormGroup({
     logName: new FormControl('Log Name'),
@@ -21,6 +36,7 @@ export class ModalComponent {
     departureTime: new FormControl(''),
     arrivalTime: new FormControl(''),
     driverName: new FormControl('Driver name'),
+    registration: new FormControl('Registration'),
     make: new FormControl('Make'),
     model: new FormControl('Model'),
   })
@@ -31,5 +47,10 @@ export class ModalComponent {
       this.form.value.driverName = 'self'
     }
     console.log(this.form.value)
+  }
+
+  select(option: ICarInfo) {
+    const newForm = { ...this.form.value, registration: option.registration, make: option.make, model: option.model }
+    this.form.setValue(newForm)
   }
 }
---
+++ src/app/interfaces/car-info.interface.ts
@@ -0,0 +1,7 @@
+export interface ICarInfo {
+  imageUrl: string;
+  registration: string;
+  model: string;
+  id: number;
+  make: string
+}
